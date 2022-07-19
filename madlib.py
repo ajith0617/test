dda7c647ac7ae229ff54d1a6d8387eef7979c934
Ajith S <ajith0617@gmail.com>
2022-07-19 12:51:23 +0530

    first

---
+++ madlib.py
@@ -0,0 +1,5 @@
+name  = input("Enter any name: ")
+school = input('Enter any school name: ')
+company = input('Enter any company name: ')
+
+print(f"Hai,My name is {name}.I was studeied in {school} school.After that I join in a {company} company")
