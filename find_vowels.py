cca26cad631dc2793428109f5c28d6580a4afa78
Ajith S <ajith0617@gmail.com>
2022-07-19 12:46:43 +0530

    first

--- find_vowels.py
+++
@@ -1,8 +0,0 @@
-word = input("Enter the word: ").lower()
-vowels = ['a','e','i','o','u']
-total = 0
-for letter in word:
-    if letter in vowels:
-        total += 1
-
-print(f"Total vowels in the word is : {total}")
--- todo/app/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from app.models import Todo
-# Register your models here.
-
-admin.site.register(Todo)
